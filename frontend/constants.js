e1d03f3cf1ca8e9fd9328d1ca233dac2d4f04b7d
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-03-22 17:28:20 -0500

    login.html added

--- frontend/constants.js
+++ frontend/constants.js
@@ -3,3 +3,4 @@ export const CASTLE_LEFT = 2;
 export const CASTLE_RIGHT = 3;
 export const pieceChoices = ["rook", "bishop", "knight", "queen"];
 export const RETRY_INTERVAL = 3000;
+export const LOGIN_URI = "http://localhost:8001/login";

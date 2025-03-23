c4222ee5c5b33d70cb3941d64b7a7b573f1c0e2d
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-03-23 15:41:30 -0500

    unit tests added for validators.py, improved exception handling in validators.py

---
+++ backend/db/tests/test_validators.py
@@ -0,0 +1,138 @@
+import pytest
+from backend.db.validators import post_refresh, post_login, post_signup, post_game_state
+from contextlib import contextmanager
+
+
+@contextmanager
+def does_not_raise():
+    yield
+
+
+class TestValidators:
+    # validators.post_refresh
+    def test_post_refresh_valid_payload(self):
+        payload = {"refresh": "refresh-token"}
+        with does_not_raise():
+            post_refresh(payload)
+
+    def test_post_refresh_invalid_payload_not_dict(self):
+        payload = ["not_a_dict"]
+        with pytest.raises(
+            ValueError, match="Invalid post_refresh payload: must be a dict"
+        ):
+            post_refresh(payload)
+
+    def test_post_refresh_invalid_payload_wrong_key_len(self):
+        payload = {"refresh": "refresh-token", "extra_key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_refresh payload: invalid amount of keys"
+        ):
+            post_refresh(payload)
+
+    def test_post_refresh_invalid_payload_no_refresh_key(self):
+        payload = {"invalid-key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_refresh payload: must contain 'refresh' key"
+        ):
+            post_refresh(payload)
+
+    # validators.post_login
+    def test_post_login_valid_payload(self):
+        payload = {"username": "test-username", "password": "test-password"}
+        with does_not_raise():
+            post_login(payload)
+
+    def test_post_login_invalid_payload_not_dict(self):
+        payload = ["not_a_dict"]
+        with pytest.raises(
+            ValueError, match="Invalid post_login payload: must be a dict"
+        ):
+            post_login(payload)
+
+    def test_post_login_invalid_payload_wrong_key_len(self):
+        payload = {
+            "username": "test-username",
+            "password": "test-password",
+            "extra_key": None,
+        }
+        with pytest.raises(
+            ValueError, match="Invalid post_login payload: invalid amount of keys"
+        ):
+            post_login(payload)
+
+    def test_post_login_invalid_payload_no_username_key(self):
+        payload = {"password": "test-password", "test_key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_login payload: must contain 'username' key"
+        ):
+            post_login(payload)
+
+    def test_post_login_invalid_payload_no_password_key(self):
+        payload = {"username": "test-username", "test_key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_login payload: must contain 'password' key"
+        ):
+            post_login(payload)
+
+    # validators.post_login
+    def test_post_signup_valid_payload(self):
+        payload = {"username": "test-username", "password": "test-password"}
+        with does_not_raise():
+            post_signup(payload)
+
+    def test_post_signup_invalid_payload_not_dict(self):
+        payload = ["not_a_dict"]
+        with pytest.raises(
+            ValueError, match="Invalid post_signup payload: must be a dict"
+        ):
+            post_signup(payload)
+
+    def test_post_signup_invalid_payload_wrong_key_len(self):
+        payload = {
+            "username": "test-username",
+            "password": "test-password",
+            "extra_key": None,
+        }
+        with pytest.raises(
+            ValueError, match="Invalid post_signup payload: invalid amount of keys"
+        ):
+            post_signup(payload)
+
+    def test_post_signup_invalid_payload_no_username_key(self):
+        payload = {"password": "test-password", "test_key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_signup payload: must contain 'username' key"
+        ):
+            post_signup(payload)
+
+    def test_post_signup_invalid_payload_no_password_key(self):
+        payload = {"username": "test-username", "test_key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_signup payload: must contain 'password' key"
+        ):
+            post_signup(payload)
+
+    # validators.post_game_state
+    def test_post_game_state_valid_payload(self):
+        payload = {"activePlayer": "white", "gameState": []}
+        with does_not_raise():
+            post_game_state(payload)
+
+    def test_post_game_state_not_dict(self):
+        payload = ["not_a_dict"]
+        with pytest.raises(
+            ValueError, match="Invalid post_game_state payload: must be a dict"
+        ):
+            post_game_state(payload)
+    
+    def test_post_game_state_wrong_key_len(self):
+        payload = {"activePlayer": "white", "gameState": [], "extra_key": None}
+        with pytest.raises(
+            ValueError, match="Invalid post_game_state payload: invalid amount of keys"
+        ):
+            post_game_state(payload)
+    
+    def test_pose_game_state_no_activeplayer_key(self):
+        payload = {"invalid-key": None, "gameState": []}
+        with pytest.raises(ValueError, match="Invalid post_game_state payload: must contain 'activePlayer' key"):
+            post_game_state(payload)
--- backend/db/validators.py
+++ backend/db/validators.py
@@ -1,41 +1,42 @@
 def post_refresh(payload):
-    if (
-        not isinstance(payload, dict)
-        or not (len(payload.keys()) == 1)
-        or not "refresh" in payload
-    ):
-        raise ValueError("Invalid post_refresh payload.")
+    if not isinstance(payload, dict):
+        raise ValueError("Invalid post_refresh payload: must be a dict")
+    if not (len(payload.keys()) == 1):
+        raise ValueError("Invalid post_refresh payload: invalid amount of keys")
+    if not "refresh" in payload:
+        raise ValueError("Invalid post_refresh payload: must contain 'refresh' key")
+
 
 def post_login(payload):
-    if (
-        not isinstance(payload, dict)
-        or not (len(payload.keys()) == 2)
-        or not "username" in payload
-        or not "password" in payload
-    ):
-        raise ValueError("Invalid post_login payload.")
+    if not isinstance(payload, dict):
+        raise ValueError("Invalid post_login payload: must be a dict")
+    if not (len(payload.keys()) == 2):
+        raise ValueError("Invalid post_login payload: invalid amount of keys")
+    if not "username" in payload:
+        raise ValueError("Invalid post_login payload: must contain 'username' key")
+    if not "password" in payload:
+        raise ValueError("Invalid post_login payload: must contain 'password' key")
+
 
 def post_signup(payload):
-    if (
-        not isinstance(payload, dict)
-        or not (len(payload.keys()) == 2)
-        or not "username" in payload
-        or not "password" in payload
-    ):
-        raise ValueError("Invalid post_signup payload.") 
+    if not isinstance(payload, dict):
+        raise ValueError("Invalid post_signup payload: must be a dict")
+    if not (len(payload.keys()) == 2):
+        raise ValueError("Invalid post_signup payload: invalid amount of keys")
+    if not "username" in payload:
+        raise ValueError("Invalid post_signup payload: must contain 'username' key")
+    if not "password" in payload:
+        raise ValueError("Invalid post_signup payload: must contain 'password' key")
 
 
 def post_game_state(payload):
-    if (
-        not isinstance(payload, dict)
-        or not (len(payload.keys()) == 2)
-        or not "activePlayer" in payload
-        or not "gameState" in payload
-    ):
-        raise ValueError("Invalid post_game_state payload.")
-
-
-
+    if not isinstance(payload, dict):
+        raise ValueError("Invalid post_game_state payload: must be a dict")
+    if not (len(payload.keys()) == 2):
+        raise ValueError("Invalid post_game_state payload: invalid amount of keys")
+    if not "activePlayer" in payload:
+        raise ValueError("Invalid post_game_state payload: must contain 'activePlayer' key")
+    if not "gameState" in payload:
+        raise ValueError("Invalid post_game_state payload: must contain 'gameState' key")
     
-
 
